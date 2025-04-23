400b868bb6a8e65473a33d967f0410ce08124517
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-23 13:12:24 +0900

    Displaying Items in a Grid

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
   return (
-    <CategoriesScreen />
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#24180f",
+  },
 });
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,16 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet } from "react-native";
 
-function CategoryGridTile({title, color}) {
+function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-            <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +18,35 @@ function CategoryGridTile({title, color}) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
